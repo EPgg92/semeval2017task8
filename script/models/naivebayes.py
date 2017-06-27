3c9797dc7c70a1700ee269917aae22399454f7de
enzopoggio <poggio.enzo@gmail.com>
2017-06-27 19:57:13 +0200

    change gaussianNB to multinomialNB

--- script/models/naivebayes.py
+++ script/models/naivebayes.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
 """
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 
 
 def create_nb_model(vecs_train, labels_train):
-    return GaussianNB().fit(vecs_train, labels_train)
+    return MultinomialNB().fit(vecs_train, labels_train)
 
 
 def nb_predict(nb_model, vecs_test, labels_test):
