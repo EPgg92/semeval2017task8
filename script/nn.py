0af137d18d1bab90a67c170ee960efc4b60197d1
enzopoggio <poggio.enzo@gmail.com>
2017-06-29 18:29:16 +0200

    -_-

--- script/nn.py
+++ script/nn.py
@@ -13,7 +13,9 @@ import sys
 
 def test_it(hyp):
     for_graph = []
+    str0 = ''
     print(hyp)
+    str0 += hyp + '\n'
     print("Extraction")
     train_vecs = np.array(futils.open_json(
         "../dataset/my_datasets/{}_train_vecs.json".format(hyp)))
@@ -27,17 +29,20 @@ def test_it(hyp):
     model = nn.create_trained_nn(train_vecs, train_labels, 1)
     epochs = 1
     loss, acc = model.evaluate(train_vecs, train_labels)
-    while acc < 0.95 and epochs < 300:
-        loss, acc = model.evaluate(train_vecs, train_labels)
+    while acc < 0.95 and epochs < 2:
         for_graph.append((epochs, loss, acc))
+        loss, acc = model.evaluate(train_vecs, train_labels)
         epochs += 1
+        print("Epochs: {}".format(epochs))
         nn.retrain_model(model, train_vecs, train_labels, 1)
     pred_label = nn.predict_nn(model, test_vecs, test_labels)
     pred_label = [(vc.convert_map_to_label(p), vc.convert_map_to_label(l))
                   for p, l in pred_label]
-    print(ms.all_mesure(pred_label))
-    print("Epochs: {}".format(epochs))
+    str0 += ms.all_mesure(pred_label)
+    str0 += "Epochs: {}".format(epochs)
     futils.create_json("../tests/for_graph/{}.json".format(hyp), for_graph)
+    with open("../tests/nn_{}.txt".format(hyp), 'w+') as stream:
+        stream.write(str0)
 
 
 def error(hyp):
